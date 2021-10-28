3691c1cb85a1d2ab104a4af28aa39feb8b57e20f
qiqizhouu02 <yz6897@nyu.edu>
2021-10-28 12:07:27 -0400

    exercises

---
+++ exerise.js
@@ -0,0 +1,17 @@
+console.log("Celsius to Fahrenheit");
+let a = 32;
+let b = 9;
+let c = 5;
+const d = (b / c);
+const celsius = 15;
+const fahrenheit = (celsius) * d + a;
+console.log(fahrenheit);
+
+console.log("play with variables");
+let number1 = 5;
+let number2 = 3;
+number1 -= 2;
+number2 += 2
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
